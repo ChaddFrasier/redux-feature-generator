3f90d5645200b75c8b26adc2a50d0e063269c2f0
ChaddFrasier <cmf339@nau.edu>
2022-02-03 23:49:13 -0700

    changed entry to app

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,4 @@
 #!/usr/bin/env node
-import yargs = require("yargs");
-import {generator} from "./generator";
+let command = require('./command')
 
-const yargs_obj =  yargs
-  .usage("Usage: <componentName> -t <template>")
-  .positional("n", { alias: "componentName", describe: "Name of the component you want to generate", type: "string", demandOption: true })
-  .option("t",{ alias: "template", describe: "The generation template to follow", type: "string", demandOption: true })
-  .argv;
-
-  try{
-    const options = {
-      componentName: yargs_obj['_'],
-      template: yargs_obj["template"]
-    };
-    const generatorObj = new generator(options);
-    // run the generator class
-    console.log(generatorObj.getAPI().test())
-  }catch(e){
-    console.log(e);
-  }
+command()
