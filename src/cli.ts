d28145fe0cbc8989201c8d8707cbcfbf69af7699
ChaddFrasier <cmf339@nau.edu>
2022-02-19 00:19:54 -0700

    make command line action async

--- src/cli.ts
+++ src/cli.ts
@@ -2,12 +2,15 @@
 import { handleGeneratorArguments } from "./lib/generator-arguments";
 import { GEN_STATUS } from "./lib/generator";
 const argv = require('minimist')(process.argv.slice(2));
-const status: GEN_STATUS = handleGeneratorArguments(argv);
-switch(status) {
-    case GEN_STATUS.SUCCESS:
-        // TODO: Print out information for the user
-        break;
-    case GEN_STATUS.FAILURE:
-        console.log("Usage:     generate-feature <customName> --template <framework>-<langauge>");
-        break;
-}
+handleGeneratorArguments(argv).then( status => {
+    switch(status) {
+        case GEN_STATUS.SUCCESS:
+            console.log("Generation Finished Successfully");
+            break;
+        case GEN_STATUS.FAILURE:
+            console.log("Usage:     generate-feature <customName> --template <framework>-<langauge>");
+            break;
+        default:
+            console.log("SYNC FAILURE")
+    }
+});
