dc7d8ece0a27282a643c834a992d4d07747108a8
ChaddFrasier <cmf339@nau.edu>
2022-02-24 19:03:57 -0700

    added basic command testing w/ jest

--- src/lib/rfgArgs.ts
+++ src/lib/rfgArgs.ts
@@ -15,33 +15,46 @@ const readArgs = (argv: string[]): ArgumentPayload => {
     let readStatus: RFG_STATUS = RFG_STATUS.NOTSET;
     const argsArr: string[] = [];
 
-    let index = 0
+    /**
+     * Fail on incorrect length of argv
+     */
+    if(argv.length === 0 || argv.length >= 4) {
+        readStatus = RFG_STATUS.ERROR
+        return createStatusPayload(readStatus)
+    }
     
-    while( readStatus == RFG_STATUS.NOTSET && argv.length > 0 && argv.length < 4 ) {
+    for (let index = 0; index < argv.length; index++) {
+        const element = argv[index];
 
-        if(index === 0 && NAME_VALIDATOR.test(argv[0]))
+        if(index === 0 && NAME_VALIDATOR.test(element))
         {
-            argsArr[0] = argv[index];
+            argsArr[index] = element;
+            readStatus = RFG_STATUS.GO
+            continue;
         }
-        switch(argv[index]){
+        switch(element){
             case "-h":
             case "--help":
                 readStatus = RFG_STATUS.HELP;
-                break;
+                return createStatusPayload(readStatus);
             case "-v":
             case "--version":
                 readStatus = RFG_STATUS.VERSION;
-                break;
+                return createStatusPayload(readStatus);
             case "-t":
             case "--template":
-                argsArr[1] = argv[index+1];
+                if(["redux-typescript", "redux-javascript"].includes(argv[++index])){
+                    argsArr[1] = argv[index];
+                } else {
+                    return createStatusPayload(RFG_STATUS.ERROR);
+                }
                 break;
-            default:
+            case "-z":
                 readStatus = RFG_STATUS.ERROR;
+                return createStatusPayload(readStatus);
         }
-
-        index++
     }
+
     return {
         status: readStatus,
         argv: ["hellow"]
@@ -52,10 +65,10 @@ const generateHelp = () => {
     let helpString = "redux-feature-generator:\t\tA simple command line tool to speed up the time it takes to setup a new redux feature.\n\n";
     helpString = helpString.concat(
         "\tUsage:\t\tgenerate-feature <featureName> [--template redux-typescript]\n\n",
+        "\tcustomeName\t\t\tThe name of your new feature.\n",
         "\t-h --help\t\t\tPrint command help.\n",
         "\t-v --version\t\t\tPrint command help.\n",
-        "\tcustomeName\t\t\tThe name of your new feature.\n",
-        "\toutPath\t\t\t\tThe path that the generated files will be placed.\n",
+        //"\toutPath\t\t\t\tThe path that the generated files will be placed.\n",
         "\t--template <template>\t\tThe template structure you would like use for generation.\n\n"
         );
     return helpString;
@@ -64,6 +77,13 @@ const generateHelp = () => {
 const getVer = (cfg: any): string => {
     return `v${cfg.version}`;
 };
+
+const createStatusPayload = (status: RFG_STATUS): ArgumentPayload => {
+    return {
+        status: status,
+        argv: []
+    }
+}
 
 export const rfgArgs = {
     read: readArgs,
