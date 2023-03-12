cb0f998abf9de042801201f04f5c35982691a470
Chadd Frasier <cmf339@nau.edu>
2023-03-12 02:26:48 -0700

    proper UTs for helpers

---
+++ src/lib/helpers.test.ts
@@ -0,0 +1,37 @@
+import * as helpers from "./helpers"
+
+describe("Helper.ts Tests", () => {
+    test("Test that the functions are exported", () => {
+        expect(helpers).toBeDefined()
+        expect(helpers.capitalize).toBeDefined()
+        expect(helpers.lowercase).toBeDefined()
+        expect(helpers.replaceAll).toBeDefined()
+        expect(helpers.validName).toBeDefined()
+    });
+
+    test("Test the capitalize function", () => {
+        expect(helpers.capitalize("dElloWorld")).toBe("DElloWorld");
+        expect(helpers.capitalize("codingStick342")).toBe("CodingStick342");
+        expect(helpers.capitalize("")).toBe("");
+    });
+
+    test("Test the capitalize function", () => {
+        expect(helpers.lowercase("DElloWorld")).toBe("dElloWorld");
+        expect(helpers.lowercase("CodingStick342")).toBe("codingStick342");
+        expect(helpers.lowercase("")).toBe("");
+    });
+
+    test("Test the capitalize function", () => {
+        expect(helpers.replaceAll("DElloWorld", "World", "Test")).toBe("DElloTest");
+        expect(helpers.replaceAll("FindAllOfTheFinds", "Find", "Test")).toBe("TestAllOfTheTests");
+        expect(helpers.replaceAll("", "Find", "Test")).toBe("");
+    });
+
+    test("Test the capitalize function", () => {
+        expect(helpers.validName("NewFeature3")).toBeTruthy()
+        expect(helpers.validName("validnameHere")).toBeTruthy()
+        expect(helpers.validName("validname^Here")).toBeFalsy()
+        expect(helpers.validName("validname.Here")).toBeFalsy()
+        expect(helpers.validName("validname(Here")).toBeFalsy()
+    });
+})
