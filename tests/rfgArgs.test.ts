1f51792734db523ab3bd6ba594e53fa3eb18c549
Chadd Frasier <cmf339@nau.edu>
2023-03-12 09:50:33 -0700

    fixed tests

--- tests/rfgArgs.test.ts
+++ tests/rfgArgs.test.ts
@@ -1,69 +1,69 @@
-import { rfgArgs, RFG_STATUS } from "../src/lib/rfgArgs"
+import { rfgApi, RFG_STATUS } from "../src/lib/rfgArgs"
 import { helpers } from "./testhelpers"
 
 const CURRENT_BUILD_VERSION = "1.4.0";
 
 describe("Arguments Object Doesn't Blowup", () => {
     test("Should not be undefined", ()=>{
-        expect(rfgArgs).toBeDefined()
+        expect(rfgApi).toBeDefined()
     });
 });
 
 describe("Testing Argument Types", () => { 
     test("Should have a read function", () => {
-        expect(typeof rfgArgs.read).toBe('function')
+        expect(typeof rfgApi.processCommand).toBe('function')
     });
 
     test("Should have a read function", ()=>{
-        expect(typeof rfgArgs.version).toBe('function')
+        expect(typeof rfgApi.getVersion).toBe('function')
     });
 
     test("Should have a read function", ()=>{
-        expect(typeof rfgArgs.help).toBe('function')
+        expect(typeof rfgApi.getHelp).toBe('function')
     });
 
     test("Should have a read function", ()=>{
-        expect(typeof rfgArgs.help()).toBe('string')
+        expect(typeof rfgApi.getHelp()).toBe('string')
     });
 });
 
 describe("Testing Command Handler", () => { 
     test("Should fail when argv is an incorrect length", ()=>{
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature namehere -t template extra stuff that should fail regularly")).status).toBe(RFG_STATUS.ERROR);
-        expect(rfgArgs.read([]).status).toBe(RFG_STATUS.ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature namehere -t template extra stuff that should fail regularly")).status).toBe(RFG_STATUS.ERROR);
+        expect(rfgApi.processCommand([]).status).toBe(RFG_STATUS.ERROR);
     });
 
     test("Should pass when argv has ideal input", ()=>{
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName")).status).toBe(RFG_STATUS.OK);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName -t redux-typescript")).status).toBe(RFG_STATUS.OK);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName -t redux-javascript")).status).toBe(RFG_STATUS.OK);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName ./src/features -t redux-javascript")).status).toBe(RFG_STATUS.OK);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName")).status).toBe(RFG_STATUS.OK);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName -t redux-typescript")).status).toBe(RFG_STATUS.OK);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName -t redux-javascript")).status).toBe(RFG_STATUS.OK);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName ./src/features -t redux-javascript")).status).toBe(RFG_STATUS.OK);
     });
 
     test("Should fail when argv has wrong flag in input", ()=>{
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName -z should-fail")).status).toBe(RFG_STATUS.UNKNOWN_FLAG_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName -t should-fail")).status).toBe(RFG_STATUS.ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName -z should-fail")).status).toBe(RFG_STATUS.UNKNOWN_FLAG_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName -t should-fail")).status).toBe(RFG_STATUS.ERROR);
     });
 
     test("Should fail when argv has incorrect path for postestion 2", ()=>{
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName should-fail")).status).toBe(RFG_STATUS.FOLDER_NOT_FOUND_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName ./americanAirlines")).status).toBe(RFG_STATUS.FOLDER_NOT_FOUND_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName should-fail")).status).toBe(RFG_STATUS.FOLDER_NOT_FOUND_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName ./americanAirlines")).status).toBe(RFG_STATUS.FOLDER_NOT_FOUND_ERROR);
     });
 
     test("Should fail when name provided does not follow naming of variables", ()=>{
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature custom-Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature custom,Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature custom.Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature custom%Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName/ -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature customName ./src/features hehujhesdufi")).status).toBe(RFG_STATUS.UNKNOWN_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature custom-Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature custom,Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature custom.Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature custom%Name -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName/ -t redux-javascript")).status).toBe(RFG_STATUS.FEATURE_NAMING_ERROR);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature customName ./src/features hehujhesdufi")).status).toBe(RFG_STATUS.UNKNOWN_ERROR);
     });
 
     test("Should return a valid run when searching for help and version", ()=>{
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature -v")).status).toBe(RFG_STATUS.VERSION);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature --version")).status).toBe(RFG_STATUS.VERSION);
-        expect(rfgArgs.version(require("../package.json"))).toBe(`v${CURRENT_BUILD_VERSION}`);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature -h")).status).toBe(RFG_STATUS.HELP);
-        expect(rfgArgs.read(helpers.injectCommand("generate-feature --help")).status).toBe(RFG_STATUS.HELP);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature -v")).status).toBe(RFG_STATUS.VERSION);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature --version")).status).toBe(RFG_STATUS.VERSION);
+        expect(rfgApi.getVersion(require("../package.json"))).toBe(`v${CURRENT_BUILD_VERSION}`);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature -h")).status).toBe(RFG_STATUS.HELP);
+        expect(rfgApi.processCommand(helpers.injectCommand("generate-feature --help")).status).toBe(RFG_STATUS.HELP);
     });
 });
